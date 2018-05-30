ecf10ca393acf8f24a76a064bb04bfdee5386ed8
Kees Kluskens <kees@webduck.nl>
2018-05-30 18:08:26 +0200

    ... and apply the same to the dev config :(

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -156,7 +156,7 @@ module.exports = {
           },
           // Compile .tsx?
           {
-            test: /\.(ts|tsx)$/,
+            test: /\.(ts|tsx|js|jsx)$/,
             include: paths.appSrc,
             use: [
               {
